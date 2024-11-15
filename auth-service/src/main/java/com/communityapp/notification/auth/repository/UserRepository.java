c8830455eeabe19e8a2b0ec8399cb61e8b2a358f
Shaik Aseeruddin <84238767+Aseer123@users.noreply.github.com>
2024-11-15 12:04:54 +0530

    Add files via upload

---
+++ auth-service/src/main/java/com/communityapp/notification/auth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.communityapp.notification.auth.repository;
+
+import com.communityapp.notification.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    List<User> findByRole(String role);
+}
